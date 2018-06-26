ce5962956c7bf19f41f1632173304dd438c9943b
TannerShmoog <tannershmoog@gmail.com>
2018-06-26 06:23:16 -0600

    First commit

---
+++ app/src/main/java/com/app/imbeat/MainActivity.java
@@ -0,0 +1,49 @@
+package com.app.imbeat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SeekBar seekBar;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        seekBar = findViewById(R.id.seekBar);
+        textView = findViewById(R.id.textView2);
+        seekBar.setPadding(0,0,0,0);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser) {
+                // TODO Auto-generated method stub
+
+                textView.setText(Integer.toString(seekBar.getProgress()));
+
+            }
+        });
+
+    }
+
+    public void onProgressChanged() {
+        // TODO Auto-generated method stub
+
+
+    }
+}
